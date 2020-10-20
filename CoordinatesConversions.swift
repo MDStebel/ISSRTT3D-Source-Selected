7c5477860ccc7c456dda81a96816620e1a8f1db1
Michael Stebel <michael@michaelstebelconsulting.com>
2020-10-20 14:39:25 -0400

    Version 6.14
    Refactored

--- CoordinatesConversions.swift
+++ CoordinatesConversions.swift
@@ -16,7 +16,7 @@ struct CoordinatesConversions {
     ///   - latitude: Latitude as a Double.
     ///   - longitude: Longitude as a Double.
     ///   - format: String containing the format to use in the conversion.
-    /// - Returns: The coordinates in deg min sec format.
+    /// - Returns: The coordinates string in deg min sec format.
     static func decimalCoordinatesToDegMinSec(latitude: Double, longitude: Double, format: String) -> String {
         
         var latSeconds = Int(latitude * 3600)
@@ -42,7 +42,7 @@ struct CoordinatesConversions {
     ///   - minutes: Minutes as a Double.
     ///   - seconds: Seconds as a Double.
     ///   - direction: Direction as a String (either "N" or "S").
-    /// - Returns: <#description#>
+    /// - Returns: Decimal representation of coordinates as a Double
     static func degMinSecCoordinatesToDecimal(degrees: Double, minutes: Double, seconds: Double, direction: String) -> Double {
         
         let sign = (direction == "S" || direction == "W") ? -1.0 : 1.0
