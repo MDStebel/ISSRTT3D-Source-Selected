b165e2a26ffcecd5d0959d9cce9eb0e323ac5c09
Michael Stebel <michael@michaelstebelconsulting.com>
2020-10-26 19:04:10 -0400

    Version 6.14
    New Feature: Photorealistic interactive 3D globe with ISS position, orbital track, and precise location of the Sun and terminator. Drag the globe to pan/rotate it. Enable/disable auto-rotate. You can also enable/disable the globe in Settings.

---
+++ CrewMemberDetailView.swift
@@ -0,0 +1,86 @@
+//
+//  CrewMemberDetailView.swift
+//  ISS Real-Time Tracker
+//
+//  Created by Michael Stebel on 11/18/17.
+//  Copyright © 2016-2020 Michael Stebel Consulting, LLC. All rights reserved.
+//
+
+import UIKit
+
+
+class CrewMemberDetailView: UIView {
+
+    
+    // MARK: - Properties
+    
+    
+    /// This will hold the Twitter URL passed from the VC, if it exists. If there's no Twitter URL, then don't show the button
+    var twitterHandleURL: String? = nil {
+        willSet {
+            twitterButton.isHidden = newValue == "" ? true : false
+        }
+    }
+    
+    private let cornerRadius: CGFloat = 15.0
+    private let shortBioBackgroundColor = UIColor(named: Theme.popupBgd)?.cgColor
+    
+    
+    // MARK: - Outlets
+    
+    
+    @IBOutlet var shortBioInforomation: UILabel!
+    @IBOutlet var shortBioName: UILabel!
+    @IBOutlet var fullBioButton: UIButton!
+    @IBOutlet var twitterButton: UIButton!
+    
+    
+    // MARK: - Methods
+
+    
+    override func layoutIfNeeded() {
+        layoutIfNeeded()
+    }
+    
+    
+    /// Open Twitter app to this crew member's Twitter profile in Twitter app or Twitter website
+    @IBAction private func goToTwitter() {
+
+        // First make sure we have a Twitter URL and can extract the handle from it and that the handle isn't blank
+        guard let twitterHandle = twitterHandleURL?.deletingPrefix("https://twitter.com/"), twitterHandle != "" else { return }
+        
+        let appURL = NSURL(string: "twitter://user?screen_name=\(twitterHandle)")!
+        let webURL = NSURL(string: "https://twitter.com/\(twitterHandle)")!
+        
+        // Open Twitter app if installed. Otherwise, open Twitter website in Safari
+        let application = UIApplication.shared
+        if application.canOpenURL(appURL as URL) {
+            application.open(appURL as URL)
+        } else {
+            application.open(webURL as URL)
+        }
+
+    }
+    
+    
+    @IBAction private func close(_ sender: Any) {
+        
+        removeFromSuperview()
+        
+    }
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = cornerRadius
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowRadius = 4.0
+        layer.shadowOpacity = 0.6
+        layer.shadowOffset = CGSize(width: 0.0, height: 3.0)
+        layer.backgroundColor = shortBioBackgroundColor
+        
+    }
+
+}
---
+++ PassesTableViewCell.swift
@@ -0,0 +1,82 @@
+//
+//  PassesTableViewCell.swift
+//  ISS Real-Time Tracker
+//
+//  Created by Michael Stebel on 6/12/18.
+//  Copyright © 2016-2020 Michael Stebel Consulting, LLC. All rights reserved.
+//
+
+import UIKit
+
+
+class PassesTableViewCell: UITableViewCell {
+    
+    
+    // MARK: - Properties
+    
+    
+    private let cornerRadius: CGFloat = 15.0
+    
+
+    // MARK: - Outlets
+    
+    
+    @IBOutlet var cellBackground: UIView!
+    
+    @IBOutlet var passDate: PassDateUILabel!
+    
+    @IBOutlet var durationLabel: UILabel!
+    @IBOutlet var magnitudeLabel: UILabel!
+    
+    @IBOutlet var startTime: UILabel!
+    @IBOutlet var maxTime: UILabel!
+    @IBOutlet var endTime: UILabel!
+    
+    @IBOutlet var startAz: UILabel!
+    @IBOutlet var maxAz: UILabel!
+    @IBOutlet var endAz: UILabel!
+    
+    @IBOutlet var startEl: UILabel!
+    @IBOutlet var maxEl: UILabel!
+    @IBOutlet var endEl: UILabel!
+    
+    @IBOutlet var startComp: UILabel!
+    @IBOutlet var maxComp: UILabel!
+    @IBOutlet var endComp: UILabel!
+    
+    @IBOutlet var ratingStarView: [UIImageView]!  // Array of stars for rating
+    
+    
+    // MARK: - Methods
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        cellBackground.layer.cornerRadius = cornerRadius
+        cellBackground.layer.shadowColor = UIColor.black.cgColor
+        cellBackground.layer.shadowRadius = 3.0
+        cellBackground.layer.shadowOpacity = 0.6
+        cellBackground.layer.shadowOffset = CGSize(width: 0.0, height: 3.0)
+        
+    }
+    
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        if selected {
+            
+            cellBackground.backgroundColor = UIColor(named: Theme.tint)
+            passDate.textColor = UIColor(named: Theme.white)
+            
+        } else {
+            
+            cellBackground.backgroundColor = UIColor(named: Theme.popupBgd)
+            passDate.textColor = UIColor(named: Theme.popupBgd)
+            
+        }
+        
+    } 
+
+}
