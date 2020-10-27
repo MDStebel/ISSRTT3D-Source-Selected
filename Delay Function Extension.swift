7cc1901b3046af26b257a18c9f79ef90ca4d7e69
Michael Stebel <michael@michaelstebelconsulting.com>
2020-10-27 03:10:43 -0400

    Version 6.14

--- Delay Function Extension.swift
+++ Delay Function Extension.swift
@@ -10,6 +10,12 @@ import UIKit
 
 extension UIViewController {
     
+    
+    /// Method to wait before executing a closure
+    /// - Parameters:
+    ///   - delay: Delay
+    ///   - closure: Closure as ()->() type. The closure is allowed to execute after the method returns
+    /// - Returns: Returns immediately after delay. Closure is executed asynchronously
     func delay(_ delay: Double, closure: @escaping () -> () ) {
         
         DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + Double(Int64(delay * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC), execute: closure)
