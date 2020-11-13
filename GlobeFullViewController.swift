ee914164912239c1aef99bfd6eb4a39e24260011
Michael Stebel <michael@michaelstebelconsulting.com>
2020-11-12 22:45:31 -0500

    Version 6.14.2
    Version 6.14.2 incorporates some refactoring, fixed bug in globe panning, and improved globe performance.
    Version 6.14 introduces 3D tracking. Features photorealistic interactive 3D globe showing the real-time ISS position, orbital track, sighting circle, the tilt of the Earth, the precise location & reflection of the Sun at the subsolar point, the exact position and seasonal angle of the terminator, plus city lights at night. Expand the globe to full-screen mode. Drag the globe to pan/rotate it. Pinch to zoom in/out. Enable/disable auto-rotate.

--- GlobeFullViewController.swift
+++ GlobeFullViewController.swift
@@ -17,7 +17,7 @@ class GlobeFullViewController: UIViewController, EarthGlobeProtocol {
     // MARK: - Properties
     
     struct Constants {
-        static let apiEndpointAString       = "https://api.wheretheiss.at/v1/satellites/25544"
+        static let apiEndpointAString       = "---"
         static let fontForTitle             = Theme.nasa
         static let segueToHelpFromGlobe     = "segueToHelpFromGlobe"
         static let segueToSettings          = "segueToSettings"
