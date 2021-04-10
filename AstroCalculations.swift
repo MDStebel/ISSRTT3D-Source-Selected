91655aa4f58932bbce2a7d69ad584505449f6266
Michael Stebel <michael@michaelstebelconsulting.com>
2021-04-10 19:45:59 -0400

    6.15.2
    Under-the-hood stuff to improve the performance of the 3D Earth model.

--- AstroCalculations.swift
+++ AstroCalculations.swift
@@ -170,10 +170,9 @@ struct AstroCalculations {
         var timeCorrection, dayCorrection, lonCorrection: Double
         
         // Time calculations for the current time
-        let now            = Date()
-        let eOT            = Double(equationOfTime(for: now))
-        let localMins      = Double(Calendar.current.component(.minute, from: now))
-        let localHour      = Double(Calendar.current.component(.hour, from: now)) + localMins / Globals.numberOfMinutesInAnHour   // The current time as a decimal value
+        let eOT            = Double(equationOfTime(for: date))
+        let localMins      = Double(Calendar.current.component(.minute, from: date))
+        let localHour      = Double(Calendar.current.component(.hour, from: date)) + localMins / Globals.numberOfMinutesInAnHour   // The current time as a decimal value
         let secondsFromGMT = Double(TimeZone.current.secondsFromGMT())
         
         // Correct for time and day relative to GMT and the International Date Line
