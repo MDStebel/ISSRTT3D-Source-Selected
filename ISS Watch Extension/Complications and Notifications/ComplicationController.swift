f8d0d09cfa7bccb0dd3069591b62e593f32338cf
Michael Stebel <michael@michaelstebelconsulting.com>
2022-09-03 12:08:53 -0400

    Version 7.11

--- ISS Watch Extension/Complications and Notifications/ComplicationController.swift
+++ ISS Watch Extension/Complications and Notifications/ComplicationController.swift
@@ -15,7 +15,7 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
 
     func getComplicationDescriptors(handler: @escaping ([CLKComplicationDescriptor]) -> Void) {
         let descriptors = [
-            CLKComplicationDescriptor(identifier: "complication", displayName: "ISS Real-Time Tracker", supportedFamilies: CLKComplicationFamily.allCases)
+            CLKComplicationDescriptor(identifier: "complication", displayName: "Track", supportedFamilies: CLKComplicationFamily.allCases)
             // Multiple complication support can be added here with more descriptors
         ]
         
