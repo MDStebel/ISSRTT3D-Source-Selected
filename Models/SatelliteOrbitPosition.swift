82e9caafd43b3e13fe3a8137806637a04c625bdf
Michael Stebel <michael@michaelstebelconsulting.com>
2022-12-16 12:08:06 -0500

    Version 7.12.1

--- Models/SatelliteOrbitPosition.swift
+++ Models/SatelliteOrbitPosition.swift
@@ -3,7 +3,7 @@
 //  ISS Real-Time Tracker 3D
 //
 //  Created by Michael Stebel on 9/13/21.
-//  Copyright © 2022 ISS Real-Time Tracker. All rights reserved.
+//  Copyright © 2022-2023 ISS Real-Time Tracker. All rights reserved.
 //
 
 import Foundation
