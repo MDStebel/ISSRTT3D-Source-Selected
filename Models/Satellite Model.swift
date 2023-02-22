9f2d663adc1ebab334a79acad2194f69a2a971ab
Michael Stebel <michael@michaelstebelconsulting.com>
2023-02-22 14:29:15 -0500

    Add files via upload

--- Models/Satellite Model.swift
+++ Models/Satellite Model.swift
@@ -31,7 +31,7 @@ public enum StationsAndSatellites: String, CaseIterable {
         }
     }
     
-    var stationName: String {
+    var satelliteName: String {
         switch self {
         case .iss  :
             return "ISS"
@@ -44,7 +44,7 @@ public enum StationsAndSatellites: String, CaseIterable {
         }
     }
     
-    var stationImage: UIImage? {
+    var satelliteImage: UIImage? {
         switch self {
         case .iss :
             return UIImage(named: Globals.issIconFor3DGlobeView)!
@@ -57,48 +57,17 @@ public enum StationsAndSatellites: String, CaseIterable {
         }
     }
     
+    var satelliteImageSmall: UIImage? {
+        switch self {
+        case .iss :
+            return UIImage(named: Globals.issIconForMapView)!
+        case .tss :
+            return UIImage(named: Globals.tssIconForMapView)!
+        case .hst :
+            return UIImage(named: Globals.hubbleIconForMapView)!
+        case .none :
+            return UIImage(named: Globals.issIconForMapView)!
+        }
+    }
+    
 }
-
-
-
-//
-///// Defines a satellite object
-//class Satellite {
-//    
-//    var name: String
-//    var imageFileName: String
-//    var orbitalInclination: Float
-//    var altitudeInKM: Float
-//    var NORADCode: Int
-//    
-//    
-//    init(name: String, image: String, orbitalInclination: Float, altitudeInKM: Float, NORADCode: Int) {
-//        self.name               = name
-//        self.imageFileName      = image
-//        self.orbitalInclination = orbitalInclination
-//        self.altitudeInKM       = altitudeInKM
-//        self.NORADCode          = NORADCode
-//    }
-//    
-//    convenience init(name: String) {
-//        self.init(name: name, image: "defaultImage.png", orbitalInclination: Globals.ISSOrbitInclinationInDegrees, altitudeInKM: 430.0, NORADCode: 0)
-//        self.name               = name
-//    }
-//    
-//}
-//
-//
-//class SpaceStation: Satellite {
-//    
-//    var crew: [Astronaut]?
-//    var nation: String
-//    var orbitalColorName: String
-//    
-//    init(name: String, image: String, orbitalInclination: Float, altitudeInKM: Float, NORADCode: Int, crew: [Astronaut]?, nation: String, orbitalColorName: String) {
-//        self.crew               = crew
-//        self.nation             = nation
-//        self.orbitalColorName   = orbitalColorName
-//        super.init(name: name, image: image, orbitalInclination: orbitalInclination, altitudeInKM: altitudeInKM, NORADCode: NORADCode)
-//    }
-//    
-//}
