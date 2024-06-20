64594fea53d55e869f4cda66b714fc3a18a4c21f
Michael Stebel <michael@michaelstebelconsulting.com>
2024-06-19 21:37:54 -0400

    Version 7.16.3

--- ISS Real-Time Tracker 3D Widget/ISS_Real_Time_Tracker_3D_Widget.swift
+++ ISS Real-Time Tracker 3D Widget/ISS_Real_Time_Tracker_3D_Widget.swift
@@ -10,9 +10,11 @@ import CoreLocation
 import SwiftUI
 import WidgetKit
 
+// MARK: - Timeline provider
+
 struct Provider: TimelineProvider {
     func placeholder(in context: Context) -> NextPass {
-        NextPass(date: Date(), passDate: Date(), startAzimuth: 216.0, startAzCompass: "SW", startElevation: 18.0, maxAzimuth: 270.0, maxElevation: 60.0, endAzimuth: 30.0, endElevation: 20.0)
+        NextPass(date: Date(), passDate: Date(), duration: 399, mag: 1.8, startAzimuth: 216.0, startAzCompass: "SW", startElevation: 18.0, maxAzimuth: 270.0, maxElevation: 60.0, endAzimuth: 30.0, endElevation: 20.0)
     }
     
     func getSnapshot(in context: Context, completion: @escaping (NextPass) -> ()) {
@@ -21,7 +23,7 @@ struct Provider: TimelineProvider {
                 let pass = apiData.passes[0]
                 let passStartDate = Date(timeIntervalSince1970: pass.startUTC)
                 let currentDate = Date()
-                let entry = NextPass(date: currentDate, passDate: passStartDate, startAzimuth: pass.startAz, startAzCompass: pass.startAzCompass, startElevation: pass.startEl, maxAzimuth: pass.maxAz, maxElevation: pass.maxEl, endAzimuth: pass.endAz, endElevation: pass.endEl)
+                let entry = NextPass(date: currentDate, passDate: passStartDate, duration: pass.duration, mag: pass.mag, startAzimuth: pass.startAz, startAzCompass: pass.startAzCompass, startElevation: pass.startEl, maxAzimuth: pass.maxAz, maxElevation: pass.maxEl, endAzimuth: pass.endAz, endElevation: pass.endEl)
                 
                 completion(entry)
             }
@@ -37,7 +39,7 @@ struct Provider: TimelineProvider {
                 let currentDate = Date()
                 for minuteOffset in 0 ..< 5 {
                     let entryDate = Calendar.current.date(byAdding: .minute, value: minuteOffset, to: currentDate)!
-                    let entry = NextPass(date: entryDate, passDate: passStartDate, startAzimuth: pass.startAz, startAzCompass: pass.startAzCompass, startElevation: pass.startEl, maxAzimuth: pass.maxAz, maxElevation: pass.maxEl, endAzimuth: pass.endAz, endElevation: pass.endEl)
+                    let entry = NextPass(date: entryDate, passDate: passStartDate, duration: pass.duration, mag: pass.mag, startAzimuth: pass.startAz, startAzCompass: pass.startAzCompass, startElevation: pass.startEl, maxAzimuth: pass.maxAz, maxElevation: pass.maxEl, endAzimuth: pass.endAz, endElevation: pass.endEl)
                     entries.append(entry)
                 }
             }
@@ -88,6 +90,8 @@ struct Provider: TimelineProvider {
 struct NextPass: TimelineEntry {
     var date: Date
     let passDate: Date
+    let duration: Int
+    let mag: Double
     let startAzimuth: Double
     let startAzCompass: String
     let startElevation: Double
@@ -96,6 +100,8 @@ struct NextPass: TimelineEntry {
     let endAzimuth: Double
     let endElevation: Double
 }
+
+// MARK: - View
 
 /// Widget view
 struct ISS_Real_Time_Tracker_3D_WidgetEntryView: View {
@@ -127,7 +133,7 @@ struct ISS_Real_Time_Tracker_3D_WidgetEntryView: View {
                 VStack(spacing: family == .systemMedium ? 3 : 5) {
                     HeaderView()
                     Spacer()
-                    DateView(date: entry.passDate)
+                    DateView(date: entry.passDate, entry: entry)
                     Spacer()
                     InfoCardView(entry: entry)
                         .offset(y: -45)
@@ -143,7 +149,7 @@ struct ContainerView: View {
             .fill(Color.issrttRed)
             .overlay(
                 LinearGradient(
-                    colors: [Color.issrttRed, Color.issrttWhite],
+                    colors: [.issrttRed, .issrttRed, .issrttWhite],
                     startPoint: .top,
                     endPoint: .bottom
                 )
@@ -168,6 +174,7 @@ struct HeaderView: View {
 
 struct DateView: View {
     var date: Date
+    var entry: Provider.Entry
     @Environment(\.widgetFamily) var family
     
     @ViewBuilder
@@ -176,7 +183,7 @@ struct DateView: View {
         case .systemSmall:
             smallView()
         case .systemMedium:
-            mediumView()
+            mediumView(with: entry)
         default:
             smallView()
         }
@@ -199,7 +206,8 @@ struct DateView: View {
     }
     
     @ViewBuilder
-    private func mediumView() -> some View {
+    private func mediumView(with data: Provider.Entry) -> some View {
+        let mag = data.mag
         HStack {
             VStack(alignment: .leading, spacing: 3) {
                 dateText
@@ -207,23 +215,26 @@ struct DateView: View {
                 monthDayText
                 Spacer(minLength: 15)
             }
-//            Spacer()
             VStack(alignment: .leading, spacing: 3) {
-                countdownText
-                Spacer()
-//                Spacer()
+                Spacer(minLength: 25)
+                VStack(alignment: .leading, spacing: -45) {
+                    passQualityView(for: mag)
+                    countdownText
+                    Spacer()
+                }
+                Spacer(minLength: 5)
             }
             Spacer()
         }
         .foregroundColor(.issrttWhite)
         .offset(x: 16)
-//        Spacer()
+        Spacer()
     }
     
     private var dateText: some View {
         Text(date.formatted(.dateTime.weekday(.wide)).uppercased())
             .font(.caption).fontWeight(.heavy)
-            .opacity(0.60)
+            .opacity(0.70)
             .offset(y: 20)
     }
     
@@ -237,9 +248,11 @@ struct DateView: View {
         let diffInMinutes = (diff.day ?? 999) * 1440 + (diff.hour ?? 0) * 60 + (diff.minute ?? 0)
         return Text("T-minus \(diffInMinutes) mins")
             .font(.subheadline).fontWeight(.bold)
+            .opacity(0.70)
             .offset(y: 46)
             .minimumScaleFactor(0.60)
     }
+
 }
 
 struct InfoCardView: View {
@@ -248,27 +261,45 @@ struct InfoCardView: View {
     
     @ViewBuilder
     var body: some View {
-        let date = Text("\(entry.passDate.formatted(date: .omitted, time: .shortened))")
-        let azi = Text("\(entry.startAzimuth, format: .number.precision(.fractionLength(0)))°")
-        let elev = Text("\(entry.startElevation, format: .number.precision(.fractionLength(1)))°")
-
-        let tmLabel = family == .systemMedium ? "  Pass Start Time:  " : " Tm: "
+        let azi     = Text("\(entry.startAzimuth, format: .number.precision(.fractionLength(0)))\(Globals.degreeSign)")
+        let cp      = Text(entry.startAzCompass)
+        let date    = Text("\(entry.passDate.formatted(date: .omitted, time: .shortened))")
+        let dur     = Text("\(entry.duration) min")
+        let elev    = Text("\(entry.startElevation, format: .number.precision(.fractionLength(1)))\(Globals.degreeSign)")
+        let mag     = Text("\(entry.mag, format: .number.precision(.fractionLength(1)))")
+        
         let azLabel = family == .systemMedium ? "  Azimuth:  " : " Az: "
-        let elLabel = family == .systemMedium ? " Elevation:  " : "El: "
-        let spacing = family == .systemMedium ? 5.0 : 0.0
+        let cpLabel = family == .systemMedium ? "  Compass:  " : " Cp: "
+        let duLabel = family == .systemMedium ? "  Duration:  " : " Du: "
+        let elLabel = family == .systemMedium ? "  Elevation:  " : " El: "
+        let mgLabel = family == .systemMedium ? "  Mag:  " : " Mg: "
+        let tmLabel = family == .systemMedium ? "  Start:  " : " St: "
+        let spacing = family == .systemMedium ? 0.0 : 0.0
         
         ZStack {
             Rectangle()
                 .foregroundColor(.issrttWhite)
                 .cornerRadius(8)
                 .frame(height: 55)
-            VStack(alignment: .leading, spacing: 0) {
-                InfoRow(icon: "clock", label: tmLabel, value: date, spacing: spacing)
-                InfoRow(icon: "safari", label: azLabel, value: azi + Text(" ") + Text(entry.startAzCompass), spacing: spacing)
-                InfoRow(icon: "angle", label: elLabel, value: elev, spacing: spacing)
+            HStack(spacing: 0) {
+                VStack(alignment: .leading, spacing: 0) {       // Column 1
+                    InfoRow(icon: "clock", label: tmLabel, value: date, spacing: spacing)
+                    InfoRow(icon: "safari", label: azLabel, value: azi, spacing: spacing)
+                    InfoRow(icon: "dot.arrowtriangles.up.right.down.left.circle", label: cpLabel, value: cp, spacing: spacing)
+                }
+                .foregroundColor(.issrttRed)
+                .padding(.horizontal, 5)
+                if family == .systemMedium {                    // Column 2 (Show this column only if in the medium size widget)
+                    VStack(alignment: .leading, spacing: 0) {
+                        InfoRow(icon: "paperplane.circle", label: elLabel, value: elev, spacing: spacing)
+                        InfoRow(icon: "stopwatch", label: duLabel, value: dur, spacing: spacing)
+                        InfoRow(icon: "rays", label: mgLabel, value: mag, spacing: spacing)
+                    }
+                    .foregroundColor(.issrttRed)
+                    .padding(.horizontal, 0)
+                }
+                Spacer()
             }
-            .foregroundColor(.issrttRed)
-            .padding(.horizontal, 5)
         }
         .padding()
     }
@@ -294,6 +325,42 @@ struct InfoRow: View {
     }
 }
 
+/// Return 1-4 stars in a view based on the magnitude of the pass
+/// - Parameter pass: The pass
+/// - Returns: A view consisting of an HStack of rating stars
+private func passQualityView(for magnitude: Double) -> some View {
+    HStack(spacing: 3) {
+        Text("Pass quality:")
+            .font(.subheadline).fontWeight(.bold)
+            .opacity(0.7)
+            .minimumScaleFactor(0.90)
+            .lineLimit(1)
+        HStack(spacing: 2) {
+            ForEach(0..<4) { star in
+                Image(star < (getNumberOfStars(forMagnitude: magnitude) ?? 0) ? .icons8StarFilledWhite : .starUnfilled)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 15)
+            }
+        }
+    }
+}
+
+/// Get the number of stars from the rating system enum
+/// If no magnitude was returned by the API, return nil
+/// - Parameters:
+///   - magnitude: pass magnitude
+/// - Returns: optional Int
+private func getNumberOfStars(forMagnitude magnitude: Double) -> Int? {
+    if magnitude != RatingSystem.unknown.rawValue {
+        return RatingSystem.numberOfRatingStars(for: magnitude)
+    } else {
+        return nil
+    }
+}
+
+// MARK: - Widget configuration
+
 struct ISS_Real_Time_Tracker_3D_Widget: Widget {
     let kind: String = "ISS_Real_Time_Tracker_3D_Widget"
     
@@ -314,9 +381,9 @@ struct ISS_Real_Time_Tracker_3D_Widget: Widget {
     }
 }
 
-#Preview(as: .systemSmall) {
+#Preview(as: .systemMedium) {
     ISS_Real_Time_Tracker_3D_Widget()
 } timeline: {
-    NextPass(date: Date(), passDate: Date(), startAzimuth: 350.8, startAzCompass: "NNW", startElevation: 22, maxAzimuth: 270.0, maxElevation: 60.0, endAzimuth: 30.0, endElevation: 20.0)
-    NextPass(date: Date(), passDate: Calendar.current.date(byAdding: .month, value: 6, to: Date())!, startAzimuth: 216.3, startAzCompass: "SW", startElevation: 18.7, maxAzimuth: 270.0, maxElevation: 60.0, endAzimuth: 30.0, endElevation: 20.0)
+    NextPass(date: Date(), passDate: Date(), duration: 399, mag: -1.2, startAzimuth: 350.8, startAzCompass: "NNW", startElevation: 22, maxAzimuth: 270.0, maxElevation: 60.0, endAzimuth: 30.0, endElevation: 20.0)
+    NextPass(date: Date(), passDate: Calendar.current.date(byAdding: .month, value: 6, to: Date())!, duration: 401, mag: -0.9, startAzimuth: 216.3, startAzCompass: "SW", startElevation: 18.7, maxAzimuth: 270.0, maxElevation: 60.0, endAzimuth: 30.0, endElevation: 20.0)
 }
